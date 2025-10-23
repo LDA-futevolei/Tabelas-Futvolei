f8320e6060f261fd4bb4a97cddf25e5a98981e88
DuckCoder1101 <crisfavaedu@gmail.com>
2025-10-23 20:02:23 -0300

    Modified vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000", // 🔥 sem barra final
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+	plugins: [react()],
+	server: {
+		port: 5173,
+		host: true,
+		strictPort: true,
+		allowedHosts: ['ligadosamigosftv.com.br', 'www.ligadosamigosftv.com.br'],
+		proxy: {
+			'/api': {
+				target: 'http://localhost:3000', // 🔥 sem barra final
+				changeOrigin: true,
+				secure: false,
+			},
+		},
+	},
 });
